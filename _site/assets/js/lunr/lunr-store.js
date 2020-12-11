99670e1757ea7f2346397577ff67be9d8c596d59
Wesley Banfield <wesleybanfield@gmail.com>
2020-12-11 19:23:43 +0100

    Copy in  initial files from https://github.com/aksakalli/fullpage-jekyll

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Home",
+        "excerpt":"   fullPage Jekyll    fullPage.js &amp; Bootstrap v4 template    ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/tabs/01-intro/",
+        "teaser": null
+      },{
+        "title": "First",
+        "excerpt":"First Content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/tabs/02-first-content/",
+        "teaser": null
+      },{
+        "title": "Second",
+        "excerpt":"Second Content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/tabs/03-second-content/",
+        "teaser": null
+      },{
+        "title": "Third",
+        "excerpt":"Third Content Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/tabs/04-third-content/",
+        "teaser": null
+      }]
